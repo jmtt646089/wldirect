ec8a5ffc325915a7c657a34f802d67a068124284
jmtt646089 <jadeseala@gmail.com>
2026-02-14 18:31:18 +1300

    Update wldirect.js

--- src/endpoints/wldirect.js
+++ src/endpoints/wldirect.js
@@ -1,9 +1,13 @@
 
 const onlinePaymentsSdk = require('onlinepayments-sdk-nodejs');
 
+
+const merchantId = 'JMI';
+
+
 const directSdk = onlinePaymentsSdk.init({
-    integrator: 'JMI',
-    host: 'payment.preprod.direct.worldline-solutions.com', 
+    integrator: merchantId, // is merchantId property missed
+    host: 'payment.preprod.direct.worldline-solutions.com', // or name it apiEndpoint
     scheme: 'https', 
     port: 443, 
     enableLogging: true, 
@@ -11,3 +15,36 @@ const directSdk = onlinePaymentsSdk.init({
     apiKeyId: '[your-api-key-id]',
     secretApiKey: '[your-secret-api-key]'
 });
+
+
+
+const sdkResponse = await directSdk.hostedCheckout.createHostedCheckout(
+    merchantId,
+    {
+        order: {
+            amountOfMoney: {
+                currencyCode: "USD",
+                amount: 2345,
+            },
+            customer: {
+                merchantCustomerId: "1234",
+                billingAddress: {
+                    countryCode: "US",
+                },
+            },
+        },
+
+        hostedCheckoutSpecificInput: {
+            variant: "testVariant",
+            locale: "en_GB",
+        },
+    },
+
+    {}
+);
+
+const partialRedirectUrl = sdkResponse.partialRedirectUrl;
+const baseUrl = "https://payment.";
+reUrl = baseUrl + partialRedirectUrl;
+
+// perform a redirection of your customer to this URL
